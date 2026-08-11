06b91143afc84c12e6965cbd82ec89b21133d521
Maaz-bin-danish <maazdanish321@gmail.com>
2026-08-11 08:59:19 +0500

    Initial OpenSky radar pipeline setup

---
+++ scripts/test_opensky_api.py
@@ -0,0 +1,69 @@
+import requests
+import pandas as pd
+
+url = "https://opensky-network.org/api/states/all"
+
+try:
+    response = requests.get(url, timeout=30)
+
+    print("Status Code:", response.status_code)
+
+    data = response.json()
+
+except requests.exceptions.RequestException as e:
+    print("Request failed:", e)
+
+print("Number of aircraft:", len(data["states"]))
+
+columns = [
+    "icao24",
+    "callsign",
+    "origin_country",
+    "time_position",
+    "last_contact",
+    "longitude",
+    "latitude",
+    "baro_altitude",
+    "on_ground",
+    "velocity",
+    "true_track",
+    "vertical_rate",
+    "sensors",
+    "geo_altitude",
+    "squawk",
+    "spi",
+    "position_source"
+]
+
+df = pd.DataFrame(data["states"], columns=columns)
+
+print("\nDataFrame:")
+print(df.head())
+
+print("\nShape:")
+print(df.shape)
+
+print("\nColumns:")
+print(df.columns.tolist())
+
+df["time_position"] = pd.to_datetime(
+    df["time_position"],
+    unit="s",
+    utc=True
+)
+
+df["last_contact"] = pd.to_datetime(
+    df["last_contact"],
+    unit="s",
+    utc=True
+)
+
+df["ingestion_timestamp"] = pd.Timestamp.now(tz="UTC")
+
+print(df[[
+    "icao24",
+    "callsign",
+    "time_position",
+    "last_contact",
+    "ingestion_timestamp"
+]].head())
